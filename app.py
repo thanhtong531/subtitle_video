02220037626f5e2811cb4204e460eb016c981a60
Ashizu <64486592+thanhtong531@users.noreply.github.com>
2022-11-12 17:59:14 +0700

    Update app.py

--- app.py
+++ app.py
@@ -116,10 +116,7 @@ def index():
         
         
         # flash("Tải tập tin {} thành công".format(file.filename))
-        if language_in == language_out:
-            os.system('python3 wav.py -i {} -s {}'.format(PATH+file.filename,language_in))
-        else:
-            os.system('python3 wav.py -i {} -s {} -d {}'.format(PATH+file.filename,language_in,language_out))
+
             
         file_output = file.filename[:file.filename.index('.mp4')] + '_output.mp4'
         shutil.move(PATH+file_output, source)
